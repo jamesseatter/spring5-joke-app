5461eaa374a1d9cc5c78cdada29230955727381d
james seatter <james.seatter@hotmail.co.uk>
2018-10-02 17:37:55 +0200

    Chuck Norris Joke Web App

--- src/main/java/eu/seatter/chucknorrisjokes/controller/JokeController.java
+++ src/main/java/eu/seatter/chucknorrisjokes/controller/JokeController.java
@@ -1,16 +1,18 @@
 package eu.seatter.chucknorrisjokes.controller;
 
 import eu.seatter.chucknorrisjokes.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class MainController {
+public class JokeController {
 
     private JokeService jokeService;
 
-    public MainController(JokeService jokeService) {
+    @Autowired
+    public JokeController(JokeService jokeService) {
         this.jokeService = jokeService;
     }
 
@@ -22,3 +24,4 @@ public class MainController {
     }
 
 }
+
--- src/main/java/eu/seatter/chucknorrisjokes/services/JokeServiceImpl.java
+++ src/main/java/eu/seatter/chucknorrisjokes/services/JokeServiceImpl.java
@@ -6,9 +6,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class JokeServiceImpl implements JokeService{
 
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
     @Override
     public String getJoke() {
-        ChuckNorrisQuotes cq = new ChuckNorrisQuotes();
-        return cq.getRandomQuote();
+        return chuckNorrisQuotes.getRandomQuote();
     }
 }
